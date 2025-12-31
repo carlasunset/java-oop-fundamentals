55169c56bf64c56efb3cc6b282cf3ecf60f01f30
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2025-12-31 20:00:54 -0300

    Add conditional structures examples

--- src/Section05_Conditional_Statements/Main.java
+++ src/Section05_Conditional_Statements/Main.java
@@ -7,30 +7,30 @@ public class Main {
 
         int x = 4;
 
-        System.out.println("Bom dia!");
+        System.out.println("Good morning!");
 
-        //condição simples
+        //simple condition
         if (x > 5) {
-            System.out.println("Boa Tarde!");
+            System.out.println("Good afternoon!");
         }
 
-        System.out.println("Boa Noite!");
+        System.out.println("Good night!");
 
         System.out.println("--------------------------");
 
         Scanner sc = new Scanner(System.in);
         int hora;
         
-        System.out.println("Que horas são?");
+        System.out.println("What time is it?");
         hora = sc.nextInt();
 
-        //condição composta
+        //compound conditionals
         if (hora < 12) {
-            System.out.println("Bom dia!");
+            System.out.println("Good morning!");
         }else if (hora < 18){
-            System.out.println("Boa Tarde!");
+            System.out.println("Good afternoon!");
         }else {
-            System.out.println("Boa Noite!");
+            System.out.println("Good night!");
         }
 
         sc.close();
