0a9eeb08b9089c95049bd25321319c8e8939c43b
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-01 14:23:37 -0300

    Refactor stream mapping to use static method reference in Product

--- src/Section18_Functional_Programming_Lambda_Expressions/function/entities/Product.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/function/entities/Product.java
@@ -25,6 +25,10 @@ public class Product {
         this.price = price;
     }
 
+    public static String staticUpperCaseName(Product product){
+        return product.getName().toUpperCase();
+    }
+
     @Override
     public String toString() {
         return "Product{" + "name='" + name + '\'' + ", price=" + String.format("%.2f", price) + '}';
