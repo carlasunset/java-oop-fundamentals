edf67714316fd3a74e8e18711c4b7a8c1cc76019
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-05 20:09:15 -0300

    Add JDBC demo to retrieve data using Statement and ResultSet

---
+++ src/Section20_JDBC_Database_Access/demo_read/application/Program.java
@@ -0,0 +1,36 @@
+package Section20_JDBC_Database_Access.demo_read.application;
+
+import Section20_JDBC_Database_Access.demo_read.db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = DB.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from department");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+    }
+}
