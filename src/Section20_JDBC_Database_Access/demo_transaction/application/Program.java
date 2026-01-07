9f9f5b8cddc321bb2e1dc3a8832e95aef94042e3
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-06 22:04:32 -0300

    Add transactional version of update demo using commit and rollback

--- src/Section20_JDBC_Database_Access/demo_transaction/application/Program.java
+++ src/Section20_JDBC_Database_Access/demo_transaction/application/Program.java
@@ -1,6 +1,8 @@
 package Section20_JDBC_Database_Access.demo_transaction.application;
 
 import Section20_JDBC_Database_Access.demo_transaction.db.DB;
+import Section20_JDBC_Database_Access.demo_transaction.db.DbException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,6 +16,8 @@ public class Program {
         try {
             connection = DB.getConnection();
 
+            connection.setAutoCommit(false); // start transaction, do not automatically confirm the sql operations
+
             statement = connection.createStatement();
 
             int  rows1 = statement.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
@@ -25,12 +29,19 @@ public class Program {
 
             int  rows2 = statement.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 
+            connection.commit(); // commit transaction, confirm the sql operations
+
             System.out.println("rows1: " + rows1);
             System.out.println("rows2: " + rows2);
 
         }
         catch (SQLException e){
-            e.printStackTrace();
+            try {
+                connection.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + ex.getMessage());
+            }
         }
         finally {
             DB.closeStatement(statement);
